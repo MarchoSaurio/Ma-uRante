f78d4e2614683250f2dd9fd8c379016cd15bd5fe
MarchoSaurio <112833842+MarchoSaurio@users.noreply.github.com>
2024-05-27 21:27:50 -0600

    Add files via upload

---
+++ js/reser.js
@@ -0,0 +1,29 @@
+document.getElementById('reservation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obtener los datos del formulario
+    const name = encodeURIComponent(document.getElementById('name').value);
+    const date = encodeURIComponent(document.getElementById('date').value);
+    const time = encodeURIComponent(document.getElementById('time').value);
+    const people = encodeURIComponent(document.getElementById('people').value);
+
+    // Crear la URL con los datos
+    const url = `https://marchosaurio.github.io/PruebaQR/confirmacion.html?name=${name}&date=${date}&time=${time}&people=${people}`;
+    // Generar el QR
+    const qrCodeContainer = document.getElementById('qrcode');
+    qrCodeContainer.innerHTML = '';  // Limpiar cualquier código QR previo
+
+    const qrCode = new QRCodeStyling({
+        width: 256,
+        height: 256,
+        data: url,
+    });
+
+    qrCode.append(qrCodeContainer);
+
+     // Mostrar el contenedor del QR
+     const qrContainer = document.getElementById('qr-container');
+     qrContainer.classList.remove('d-none');
+
+
+});
